09666a1d1c4f7a527df00f95f910448cf49a2e6c
nklimashov <nklimashov@yandex.ru>
2024-05-04 17:08:14 +0300

    update

--- NewsPortal/NPortal/settings.py
+++ NewsPortal/NPortal/settings.py
@@ -154,3 +154,13 @@ ACCOUNT_AUTHENTICATION_METHOD = 'email'
 ACCOUNT_EMAIL_VERIFICATION = 'none'
 
 ACCOUNT_FORMS = {"signup": "accounts.forms.CustomSignupForm"}
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.yandex.ru'
+EMAIL_PORT = 465
+EMAIL_HOST_USER = 'test.SF.sites@yandex.ru'
+EMAIL_HOST_PASSWORD = 'svrdaohjsclufeyn'
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
+
+DEFAULT_FROM_EMAIL = "test.SF.sites@yandex.ru"
--- NewsPortal/accounts/forms.py
+++ NewsPortal/accounts/forms.py
@@ -1,10 +1,22 @@
 from allauth.account.forms import SignupForm
 from django.contrib.auth.models import Group
+from django.core.mail import EmailMultiAlternatives
 
 
 class CustomSignupForm(SignupForm):
     def save(self, request):
         user = super().save(request)
-        common_users = Group.objects.get(name="common users")
-        user.groups.add(common_users)
+        # common_users = Group.objects.get(name="common users")
+        # user.groups.add(common_users)
+        subject = 'Добро пожаловать!'
+        text = f'{user.username}, вы успешно зарегистрировались!'
+        html = (
+            f'<b>{user.username}</b>, вы успешно зарегистрировались на '
+            f'<a href="http://127.0.0.1:8000/news">сайте</a>!'
+        )
+        msg = EmailMultiAlternatives(
+            subject=subject, body=text, from_email=None, to=[user.email]
+        )
+        msg.attach_alternative(html, "text/html")
+        msg.send()
         return user
