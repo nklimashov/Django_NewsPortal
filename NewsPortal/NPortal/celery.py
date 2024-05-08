c7253421d4bc004769567b30be6aea1d9897c19a
nklimashov <nklimashov@yandex.ru>
2024-05-08 16:51:35 +0300

    add tasks

---
+++ NewsPortal/NPortal/celery.py
@@ -0,0 +1,19 @@
+import os
+import datetime
+from celery import Celery
+from celery.schedules import crontab
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'NPortal.settings')
+
+app = Celery('NPortal')
+app.config_from_object('django.conf:settings', namespace='CELERY')
+
+app.autodiscover_tasks()
+
+app.conf.beat_schedule = {
+    'posts_for_the_week_every_monday': {
+        'task': 'news.tasks.show_new_post',
+        'schedule': crontab(minute='0', hour='8', day_of_week='monday'),
+        'args': ()
+    }
+}
---
+++ NewsPortal/news/tasks.py
@@ -0,0 +1,56 @@
+from django.contrib.auth.models import User
+from django.core.mail import EmailMultiAlternatives
+from django.template.loader import render_to_string
+from celery import shared_task
+from .models import Post
+from django.conf import settings
+import datetime
+
+
+@shared_task
+def post_create(post_pk, *args, **kwargs):
+    instance = Post.objects.get(pk=post_pk)
+    category = instance.category.all()
+    users = User.objects.filter(subscriptions__category__in=category).distinct()
+    emails = [user.email for user in users]
+
+    subject = f'Новый пост в категории/категориях: {[i for i in instance.category.all()]}'
+
+    text = (
+        f'Ссылка на пост: http://127.0.0.1:8000/posts/{instance.get_absolute_url()}'
+        f'Пост: {instance.category_type}'
+        f'Автор: {instance.post_author}'
+        f'Название: {instance.title}'
+    )
+
+    html = (
+        f'--<a href=http://127.0.0.1:8000/posts/{instance.get_absolute_url()}>Ссылка на пост</a>--<br><br>'
+        f'Пост: {instance.category_type}<br>'
+        f'Автор: {instance.post_author}<br>'
+        f'Название: {instance.title}'
+    )
+
+    msg = EmailMultiAlternatives(subject=subject, body=text, from_email=settings.DEFAULT_FROM_EMAIL, to=emails)
+    msg.attach_alternative(html, 'text/html')
+    msg.send()
+
+
+@shared_task
+def show_new_post(*args, **kwargs):
+    today = datetime.datetime.now()
+    last_week = today - datetime.timedelta(days=7)
+    posts = Post.objects.filter(date_time__gte=last_week)
+    categories_posts = posts.values_list('category', flat=True)
+    subscribers = User.objects.filter(subscriptions__category__in=categories_posts).values_list('email', flat=True)
+
+    subject = 'Сводка новостей за неделю'
+    html = render_to_string(
+        template_name='week_news.html',
+        context={
+            'posts': posts
+        }
+    )
+
+    msg = EmailMultiAlternatives(subject=subject, body='', from_email=settings.DEFAULT_FROM_EMAIL, to=subscribers)
+    msg.attach_alternative(html, 'text/html')
+    msg.send()
